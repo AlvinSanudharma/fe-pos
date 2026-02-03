aa9a95a0ea549b04bf13c731ba82203a61691adc
AlvinSanudharma <alvinsanudharma@gmail.com>
2026-02-03 20:19:11 +0800

    Layouting 1

---
+++ src/layout/AppSidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="fixed left-0 top-0 h-full w-64 bg-white border-surface-200 flex flex-col z-50 transition-all duration-300"
+  >
+    <!-- Logo -->
+    <div class="h-20 px-8 border-b border-surface-100 flex items-center">
+      <div
+        class="w-8 h-8 bg-primary-600 rounded-lg flex items-center justify-center text-white mr-3"
+      >
+        <i class="pi pi-bolt text-lg"></i>
+      </div>
+      <span class="text-xl font-bold text-surface-900">BWA POS</span>
+    </div>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import AppLayout from "@/layout/AppLayout.vue";
+import Dashboard from "@/pages/Dashboard.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      component: AppLayout,
+      children: [
+        {
+          path: "/",
+          name: "Dashboard",
+          component: Dashboard,
+        },
+      ],
+    },
+  ],
+});
 
-export default router
+export default router;
