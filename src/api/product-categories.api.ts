af2b043f71bdcf12fe42993cc7faf23cbb059d22
AlvinSanudharma <alvinsanudharma@gmail.com>
2026-05-17 14:41:05 +0800

    Create Category

--- src/api/product-categories.api.ts
+++ src/api/product-categories.api.ts
@@ -6,5 +6,10 @@ export const getCategories = async (params: {
   limit?: number;
 }) => api.get("/product-categories", { params });
 
+export const createCategory = async (payload: {
+  name: string;
+  description?: string;
+}) => api.post("/product-categories", payload);
+
 export const deleteCategory = async (id: number) =>
   api.delete(`/product-categories/${id}`);
--- src/pages/product-categories/CategoryForm.vue
+++ src/pages/product-categories/CategoryForm.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import { Button, FileUpload } from "primevue";
+import { createCategory } from "@/api/product-categories.api";
+import router from "@/router";
+import {
+  Button,
+  FileUpload,
+  InputText,
+  Message,
+  Textarea,
+  Toast,
+  useToast,
+} from "primevue";
 import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+
+const toast = useToast();
+
+const loading = ref(false);
+const errors = ref<Record<string, string[]>>({});
+
+const form = ref({
+  name: "",
+  description: "",
+});
 
 const categoryId = computed<number | null>(() => {
   return route.params.id ? Number(route.params.id) : null;
@@ -21,6 +41,41 @@ const onFileSelect = (event: { files: File[] }) => {
 
   selectedFile.value = file;
   imagePreview.value = URL.createObjectURL(file);
+};
+
+const submit = async () => {
+  loading.value = true;
+
+  try {
+    await createCategory(form.value);
+
+    toast.add({
+      severity: "success",
+      summary: "Category Created",
+      detail: `The category "${form.value.name}" has been successfully created.`,
+      life: 1000,
+    });
+
+    setTimeout(() => {
+      router.push("/product-categories");
+    }, 1000);
+  } catch (error: any) {
+    if (error.response?.status === 422) {
+      errors.value = error.response.data.errors || {};
+    } else {
+      // Handle other types of errors (e.g., network errors)
+      toast.add({
+        severity: "error",
+        summary: "Create Category Failed",
+        detail:
+          error.response?.data?.message ||
+          "An unexpected error occurred. Please try again.",
+        life: 1000,
+      });
+    }
+  } finally {
+    loading.value = false;
+  }
 };
 </script>
 
@@ -49,7 +104,7 @@ const onFileSelect = (event: { files: File[] }) => {
       </Button>
     </div>
     <div class="bg-white rounded-2xl border border-surface-200 overflow-hidden">
-      <form action="">
+      <form @submit.prevent="submit">
         <div
           class="p-6 md:p-8 grid grid-cols-1 md:grid-cols-12 gap-8 md:gap-12"
         >
@@ -89,8 +144,72 @@ const onFileSelect = (event: { files: File[] }) => {
               </small>
             </div>
           </div>
+          <div class="md:col-span-8 flex flex-col gap-6">
+            <div class="flex flex-col gap-2">
+              <label for="email" class="font-medium text-surface-900"
+                >Name <span class="text-red-500">*</span></label
+              >
+              <InputText
+                type="text"
+                id="name"
+                fluid
+                class="bg-surface-50! focus:bg-white! border-surface-200"
+                v-model="form.name"
+                placeholder="Enter category name"
+                :invalid="!!errors.name"
+              />
+            </div>
+            <Message
+              v-if="errors.name"
+              severity="error"
+              size="small"
+              variant="simple"
+              >{{ errors.name[0] }}</Message
+            >
+            <div class="flex flex-col gap-2">
+              <label for="email" class="font-medium text-surface-900"
+                >Description <span class="text-red-500">*</span></label
+              >
+              <Textarea
+                type="text"
+                id="description"
+                fluid
+                class="bg-surface-50! focus:bg-white! border-surface-200"
+                v-model="form.description"
+                placeholder="Enter category description"
+                rows="6"
+              />
+            </div>
+
+            <div
+              class="flex justify-end pt-6 mt-auto border-t border-surface-100"
+            >
+              <div class="flex gap-3">
+                <Button
+                  label="Cancel"
+                  severity="secondary"
+                  @click="router.back()"
+                />
+                <Button
+                  v-if="!loading"
+                  type="submit"
+                  label="Save Category"
+                  class="p-button-primary"
+                  icon="pi pi-check"
+                />
+                <Button
+                  v-else
+                  type="submit"
+                  label="Loading..."
+                  class="p-button-primary"
+                  icon="pi pi-check"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </form>
     </div>
   </div>
+  <Toast />
 </template>
